16fd093b1d058ff382c34e3a7d3eb14d927cf108
Daryll Born <born.dd@gmail.com>
2021-10-08 01:21:03 +0200

    Created Stripe route

--- index.js
+++ index.js
@@ -7,6 +7,7 @@ const authRoute = require("./routes/auth");
 const productRoute = require("./routes/product");
 const cartRoute = require("./routes/cart");
 const orderRoute = require("./routes/order");
+const stripeRoute = require("./routes/stripe");
 
 dotenv.config();
 
@@ -28,3 +29,4 @@ app.use("/api/users", userRoute);
 app.use("/api/products", productRoute);
 app.use("/api/carts", cartRoute);
 app.use("/api/orders", orderRoute);
+app.use("/api/checkout", stripeRoute);
