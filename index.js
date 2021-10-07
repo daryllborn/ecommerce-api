9c1e7de9e5dc7fb366683a57765042d656ea81dc
Daryll Born <born.dd@gmail.com>
2021-10-07 22:56:36 +0200

    Created authorization route

--- index.js
+++ index.js
@@ -3,6 +3,10 @@ const app = express();
 const mongoose = require("mongoose");
 const dotenv = require("dotenv");
 const userRoute = require("./routes/user")
+const authRoute = require("./routes/auth");
+const productRoute = require("./routes/product");
+const cartRoute = require("./routes/cart");
+const orderRoute = require("./routes/order");
 
 dotenv.config();
 
@@ -19,4 +23,8 @@ mongoose
 
 app.use(express.json());
 
-app.use("/api/user", userRoute);
+app.use("/api/auth", authRoute);
+app.use("/api/users", userRoute);
+app.use("/api/products", productRoute);
+app.use("/api/carts", cartRoute);
+app.use("/api/orders", orderRoute);
