3fda08c0b515e0da9cedf20dd7e126891a2fc318
Daryll Born <born.dd@gmail.com>
2021-10-14 21:44:31 +0200

    Added middleware to fix cors acces control

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ const stripeRoute = require("./routes/stripe");
 dotenv.config();
 
 app.listen(process.env.PORT || 5000, () => {
-  console.log("Backend server is running");
+  console.log("Backend server is running on port: ");
 });
 
 mongoose
@@ -22,7 +22,28 @@ mongoose
     console.log(err);
   });
 
+  app.use(function (req, res, next) {
+
+    // Website you wish to allow to connect
+    res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000');
+
+    // Request methods you wish to allow
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+
+    // Request headers you wish to allow
+    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+
+    // Set to true if you need the website to include cookies in the requests sent
+    // to the API (e.g. in case you use sessions)
+    res.setHeader('Access-Control-Allow-Credentials', true);
+
+    // Pass to next layer of middleware
+    next();
+});
+
 app.use(express.json());
+
+
 
 app.use("/api/auth", authRoute);
 app.use("/api/users", userRoute);
